628ecf59a03a9032a372f506a839fab283280ce7
leonardoneves <leonardo.neves@m4u.com.br>
2018-01-29 14:17:23 -0200

    :cow: val cannot be reassign

--- kotlin-udemy-demo/src/FirstApp.kt
+++ kotlin-udemy-demo/src/FirstApp.kt
@@ -1,7 +1,10 @@
 
 fun main(args:Array<String>) {
     val first_name:String
-    first_name = "Leo"
-    var last_name = "Neves"
-    print("Welcome to Kotlin, $first_name $last_name")
+    first_name = "Edson"
+    var last_name = "Neves errado"
+    last_name = "Neves"
+    val second_name = "Leonardo"
+    // second_name = "leo" val cannot be reassign
+    print("Welcome to Kotlin, $first_name $second_name $last_name")
 }
