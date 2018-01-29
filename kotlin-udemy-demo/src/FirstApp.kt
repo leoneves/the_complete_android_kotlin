c2fed933420f0846ec59f8bda787ab9ef7d23af6
leonardoneves <leonardo.neves@m4u.com.br>
2018-01-29 14:13:34 -0200

    :cow: declare immutable first_name

--- kotlin-udemy-demo/src/FirstApp.kt
+++ kotlin-udemy-demo/src/FirstApp.kt
@@ -1,6 +1,6 @@
 
 fun main(args:Array<String>) {
-    var first_name:String
+    val first_name:String
     first_name = "Leo"
     var last_name = "Neves"
     print("Welcome to Kotlin, $first_name $last_name")
